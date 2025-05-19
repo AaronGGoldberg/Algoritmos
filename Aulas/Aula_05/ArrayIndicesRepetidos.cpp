deee0c6249a8a3a24953656c85d6d184eb2acc94
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-05-19 07:29:18 +0000

    Questão sobre Repetição de Índices

--- Aulas/Aula_05/ArrayIndicesRepetidos.cpp
+++ Aulas/Aula_05/ArrayIndicesRepetidos.cpp
@@ -2,17 +2,25 @@
 
 void contarOcorrencias(int A[], int n){
     int C[n];
-    for(int i = 0; i < n; ++i){
+    for (int i = 0; i < n; ++i){
         C[i] = 1;
-        for(int j = 0; j < n; ++j){
-            if (A[i] == A[j+1]){
+    }
+    for(int i = 0; i < n; ++i){
+        if(C[i] == 0){
+            continue;
+        }
+        std::cout << "Ocorrências " << i << std::endl;
+        for(int j = i + 1; j < n; ++j){
+            if (A[i] == A[j]){
                 C[i] = C[i] + 1;
+                C[j] = 0;
+                std::cout << C[i] << " " << i << " " << j << std::endl;
             }
         }
     }
     for(int i = 0; i < n; ++i){
         if (C[i] > 1){
-            std::cout << "O Elemento " << A[i] << " Ocorre " << C[i] << 'vezes' << std::endl; 
+            std::cout << "O Elemento " << A[i] << " Ocorre " << C[i] << " vezes" << std::endl; 
         }
     }
 }
