b467ac3f6261db91410de23b0025605db3f186fc
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-05-19 07:55:34 +0000

    Array Índices Repetidos Verificação

--- Aulas/Aula_05/ArrayIndicesRepetidos.cpp
+++ Aulas/Aula_05/ArrayIndicesRepetidos.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
-
+//O(n^2)
 void contarOcorrencias(int A[], int n){
     int C[n];
-    for (int i = 0; i < n; ++i){
-        C[i] = 1;
-    }
     for(int i = 0; i < n; ++i){
-        if(C[i] == 0){
+        if(C[i] == 0){ //verifica se o elementos atual da lista ja foi contado
             continue;
         }
-        std::cout << "Ocorrências " << i << std::endl;
+        C[i] = 1;
+
         for(int j = i + 1; j < n; ++j){
             if (A[i] == A[j]){
                 C[i] = C[i] + 1;
-                C[j] = 0;
-                std::cout << C[i] << " " << i << " " << j << std::endl;
+                C[j] = 0; //Marca o indice da lista que ja foi contado
             }
         }
     }
+
     for(int i = 0; i < n; ++i){
         if (C[i] > 1){
             std::cout << "O Elemento " << A[i] << " Ocorre " << C[i] << " vezes" << std::endl; 
@@ -28,9 +26,15 @@ void contarOcorrencias(int A[], int n){
 int main(){
     int n;
     std::cin >> n;
+    if (n < 1 && n > 1000000){
+        std::cout << "Erro de entrada, valor inválido!!!";
+    }
     int A[n];
     for(int i = 0; i < n; ++i){
         std::cin >> A[i];
+        if (A[i] < 0 && A[i] > 100000){
+            std::cout << "Valor Inválido!";
+        }
     }
     contarOcorrencias(A, n);
     return 0;
