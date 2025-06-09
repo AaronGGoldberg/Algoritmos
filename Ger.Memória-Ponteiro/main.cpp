2ec06a915fe89cbe83cbb154ea2537cb251e90c9
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-06-09 18:28:29 +0000

    Gerenciamento de Memória com Ponteiro

--- Ger.Memória-Ponteiro/main.cpp
+++ Ger.Memória-Ponteiro/main.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
 #include <chrono>
-const int INITIAL_CAPACITY = 100, INCREASE_CAPACITY = 100;
 
-int∗ increase_capacity(int∗ data, int& capacity, int& size) {
-    int∗ new_array = new int[capacity + INCREASE_CAPACITY];
+const int INITIAL_CAPACITY = 100;
+const int INCREASE_CAPACITY = 100;
+
+// Função para aumentar a capacidade do vetor
+int* increase_capacity(int* data, int& capacity, int& size) {
+    int* new_array = new int[capacity + INCREASE_CAPACITY];
     for (int i = 0; i < size; ++i)
         new_array[i] = data[i];
-    int∗ old_array = data;
-    delete[] old_array;
-    capacity = capacity + INCREASE_CAPACITY;
+    
+    delete[] data;
+    capacity += INCREASE_CAPACITY;
     return new_array;
 }
-int∗ push_back(int∗ data, int& capacity, int& size, int value) {
+
+// Função para inserir elemento no final do vetor
+int* push_back(int* data, int& capacity, int& size, int value) {
     if (size == capacity)
         data = increase_capacity(data, capacity, size);
+    
     data[size++] = value;
     return data;
 }
+
 int main() {
-    int∗ data;
-    int size = 0, capacity = INITIAL_CAPACITY, x;
+    int* data;
+    int size = 0;
+    int capacity = INITIAL_CAPACITY;
+    int x;
+
     auto beg = std::chrono::high_resolution_clock::now();
+
     data = new int[capacity];
-    while (std::cin >> x, x != −1)
+
+    while (std::cin >> x, x != -1)
         data = push_back(data, capacity, size, x);
+
     auto end = std::chrono::high_resolution_clock::now();
+
     std::cout << "Quantidade de números lidos: " << size << std::endl;
     std::cout << "Tamanho do vetor: " << capacity << std::endl;
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end − beg);
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
     std::cerr << "Tempo de processamento: " << duration.count() << " microseconds(s)" << std::endl;
-    delete[] data; // Free the allocated memory
+
+    delete[] data; // Libera a memória alocada
+
     return 0;
 }
